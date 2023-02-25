7e6746d4520eaca1f64cf5e5634055c588105780
Covadonga M <cova.millan@gmail.com>
2023-02-25 20:47:20 +0100

    add lines to the class

--- src/repository/jokes.file.repo.ts
+++ src/repository/jokes.file.repo.ts
@@ -1,15 +1,37 @@
 import fs from 'fs/promises';
+import { Joke } from '../models/jokes';
 
-const file = './data/data.json';
+const file = './data/jokes.json';
 
 export class JokesFileRepo {
-  read() {
+  read(): Promise<Joke[]> {
     return fs
       .readFile(file, { encoding: 'utf-8' })
-      .then((data) => JSON.parse(data) as any[]);
+      .then((data) => JSON.parse(data));
   }
 
-  write() {}
+  async readById(id: Joke['id']) {
+    const initialData = await fs.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const data: Joke[] = JSON.parse(initialData);
+    return data.filter((item) => item.id === id)[0];
+  }
+
+  async write(joke: Joke) {
+    const initialData = await fs.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const data: Joke[] = JSON.parse(initialData);
+    const newId: number =
+      data.length > 0 ? Math.max(...data.map((item) => item.id)) : 0;
+    joke.id = newId + 1;
+    const newData = JSON.stringify([...data, joke]);
+    await fs.writeFile(file, newData, {
+      encoding: 'utf-8',
+    });
+  }
+
   update() {}
   delete() {}
 }
